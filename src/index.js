5a2bbb0a3befd299cc3d292f61d214a2b9e50e65
gustavoluchi <gustavo.luchi@gmail.com>
2021-02-18 22:33:17 -0300

    Tirar os logos e adicionar a paleta de cores

--- src/index.js
+++ src/index.js
@@ -11,6 +11,8 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+// paleta de cores 264653,2a9d8f,e9c46a,f4a261,e76f51
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
