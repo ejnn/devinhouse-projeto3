8922159028e6d22d71294c807324235397c24c22
ejnn <eduardojorgenicolau@gmail.com>
2021-02-23 13:14:22 -0300

    store provider now in index.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
-import { Provider } from "react-redux";
-import store from "redux/store.js";
-
 function App() {
   return (
-      <Provider store={store}>
-      </Provider>
+      <div> ~ Shopee ~ </div>
   );
 }
 
