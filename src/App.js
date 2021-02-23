04c36076c7cc8d12136b8f5aad8f3d8a683e7b93
ejnn <eduardojorgenicolau@gmail.com>
2021-02-23 16:08:09 -0300

    styled-components theme injection

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import { useSelector } from "react-redux";
+import { themeSelector } from "redux/slices/theme.js";
+import { ThemeProvider } from "styled-components";
+
 function App() {
-  return (
-      <div> ~ Shopee ~ </div>
-  );
+    const theme = useSelector(themeSelector());
+
+    return (
+	<ThemeProvider theme={theme}>
+	    <div> ~ Shopee ~ </div>
+	</ThemeProvider>
+    );
 }
 
 export default App;
