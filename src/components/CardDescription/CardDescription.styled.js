cd7625559489dd865725819aede09184dc2b0819
David Mendonça <davidmed66@gmail.com>
2021-02-24 11:43:01 -0300

    componente card description criado

---
+++ src/components/CardDescription/CardDescription.styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Description = styled.span`
+  font-family: Roboto;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 24px;
+  letter-spacing: 0em;
+  text-align: left;
+`;
---
+++ src/components/CardDescription/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { 
+    CardWrapper,
+    PriceWrapper, 
+    IntegerPartOfPrice, 
+    DecimalPartOfPrice, 
+    SubTotal
+} from "../Card/CardWrapper.styled";
+import { Description} from "./CardDescription.styled";
+
+
+export default function CardDescription() {
+    return (
+        <CardWrapper />
+    )
+}
+
+
