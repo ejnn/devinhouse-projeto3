1fdbda6d7f1c94f57f0f65637d853d4b4a135c28
GuiNoronhaS <guinoronha7@gmail.com>
2021-02-23 21:12:25 -0300

    Botao Funcionando

--- src/components/BuyButton/BuyButton.styled.js
+++ src/components/BuyButton/BuyButton.styled.js
@@ -4,6 +4,8 @@ const BuyButton = styled.div`
     display: flex;
     width: 226px;
     justify-content: space-between;
+    align-items: center;
+    font-size: 2rem;
 `;
 
 export default BuyButton;
--- src/components/BuyButton/index.js
+++ src/components/BuyButton/index.js
@@ -1,26 +1,35 @@
-import TrashIcon from "assets/trash icon.svg"
-import Button from "src/components/Button.styled.js"
+import TrashIcon from "assets/trash icon.svg";
+import Button from "components/Button.styled";
+import BuyButton from "./BuyButton.styled";
 import { itemCountSelector, addItem, removeItem } from "redux/slices/shoppingCart";
 
-export default function index(props) {
-    const [id] = props;
+import { useState } from "react"
 
-    const itemCountInCart = itemCountSelector(props.id);
+export default function Index(props) {
+    const { id } = props;
+
+    const [teste, setteste] = useState(0)
+
+    const itemCountInCart = itemCountSelector(id);
     const addInCart = addItem(props);
     const removeFromCart = removeItem(props);
 
-    return (
-        itemCountInCart => 1 ?
-            <>
-                <Button width="44px" onClick={removeFromCart}>
-                    {1 < itemCountInCart} ?
-                    <img src={TrashIcon} alt="Trash Can Icon"/> 
-                    : -
+    const content = () => {
+        if (teste >= 1) {
+            return (<>
+                <Button width="44px" onClick={() => setteste(teste - 1)}>
+                    {(1 == teste) ? <img src={TrashIcon} alt="Trash Can Icon" /> : <span> - </span>}
                 </Button>
-                <div> itemCountInCart </div>
-                <Button width="44px" onClick={addInCart}> + </Button>
-            </>
-            :
-            <Button onClick={addInCart}> Comprar </Button>
+                <div> {teste} </div>
+                <Button width="44px" onClick={() => setteste(teste + 1)}> + </Button>
+            </>);
+        } else {
+            return <Button onClick={() => setteste(teste + 1)}> Comprar </Button>;
+        }
+    }
+    return (
+        <BuyButton>
+            {content()}
+        </BuyButton>
     );
 }
