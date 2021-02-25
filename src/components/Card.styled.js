6fd4fbb9990e930604821bbde5304919c0d42a13
David Mendonça <davidmed66@gmail.com>
2021-02-24 22:59:48 -0300

    arquivo de estilização dos cards

---
+++ src/components/Card.styled.js
@@ -0,0 +1,32 @@
+/* imagem, pricewraper, pricecomplement, integerpartofprice */
+import styled from "styled-components";
+
+export const Image = styled.img`
+  height: 226px;
+  width: 226px;
+  left: 172px;
+  top: 200px;
+  border-radius: 0px;
+`;
+
+export const PriceWrapper = styled.div`
+  left: 172px;
+  top: 506px;
+  white-space: nowrap;
+  width: 100%;
+`;
+
+export const PriceComplement = styled.span`
+  font-size: 14px;
+  line-height: 24px;
+`;
+
+export const IntegerPartOfPrice = styled.span`
+  font-size: 32px;
+  line-height: 24px;
+`;
+
+export const Parcela = styled.span`
+  font-size: 12px;
+  line-height: 24px;
+`;
