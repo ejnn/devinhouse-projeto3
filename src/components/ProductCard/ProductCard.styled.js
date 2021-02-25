d3db81df19f7d503854136df909c330af57eec65
David Mendonça <davidmed66@gmail.com>
2021-02-24 23:04:38 -0300

    exclusão de componentes

--- src/components/ProductCard/ProductCard.styled.js
+++ src/components/ProductCard/ProductCard.styled.js
@@ -11,15 +11,10 @@ export const CardWrapper = styled.div`
   box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.13);
   display: flex;
   flex-direction: column;
-`;
-
-export const Image = styled.img`
-  height: 226px;
-  width: 226px;
-  left: 172px;
-  top: 200px;
-  border-radius: 0px;
-  margin-bottom: 20px
+  padding: 16px;
+  justify-content: space-between;
+  align-items: center;
+  box-sizing: border-box;
 `;
 
 export const Title = styled.span`
@@ -32,31 +27,10 @@ export const Title = styled.span`
   line-height: 24px;
   letter-spacing: 0em;
   text-align: left;
-  margin-bottom: 20px;
-
+  overflow: hidden; 
+  
   /*   
   max de duas linhas. Truncar dps disso
-  white-space: nowrap;
-  overflow: hidden;
-  text-overflow: ellipsis; */
-`;
-
-export const PriceWrapper = styled.div`
-  left: 172px;
-  top: 506px;
-  white-space: nowrap;
-  margin-bottom: 20px
-`;
-
-export const PriceComplement = styled.span`
-  font-size: 14px;
-  line-height: 24px;
-`;
-
-export const IntegerPartOfPrice = styled.span`
-  font-size: 32px;
-  line-height: 24px;
-`;
-
-export const Parcela = styled.span`
+  text-overflow: ellipsis;
+  white-space: nowrap;*/
 `;
