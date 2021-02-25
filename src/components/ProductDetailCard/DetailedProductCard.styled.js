c04c523ebe1834e4f5441644a2606869f2a26082
David Mendonça <davidmed66@gmail.com>
2021-02-24 23:02:47 -0300

    alteração de nome e alterações

---
+++ src/components/ProductDetailCard/DetailedProductCard.styled.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const ProductDetailCardWrapper = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  height: 260px;
+  width: 1128px;
+  left: 156px;
+  top: 190px;
+  border-radius: 7px;
+  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.3);
+  padding: 32px
+`;
+
+export const DetailWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  margin-left: 32px;
+`;
+
+export const DescriptionWrapper = styled.div`
+  size: 16px;
+  line-height: 24px;
+`;
