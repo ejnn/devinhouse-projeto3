1c3faae01144f7df0125e3d6217fa5c48e0da68a
Murilo Sartori <m.sartori22@gmail.com>
2021-02-25 22:06:40 -0300

    criar os componentes do carrinho

---
+++ src/components/Cart/ProductTable/ProductTable.styled.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const SBoxCart = styled.div`
+  display: flex;
+  width: 100%;
+  min-height: 446px;
+  background: #FFF;
+  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.13);
+  border-radius: 7px;
+  table {
+    width: 100%;
+    padding: 7px;
+  }
+`;
+
+export const SEmptyCart = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  align-content: center;
+  text-align: center;
+  width: 100%;
+  font-size: 32px;
+  font-weigth: 500;
+`;
