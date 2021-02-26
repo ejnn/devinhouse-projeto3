2b5a1ce15d0578e7f7ac1dbbe93e6b94f415611d
gustavoluchi <gustavo.luchi@gmail.com>
2021-02-25 21:58:26 -0300

    vai eduardo

--- src/components/Header/ShoppingCartButton/index.js
+++ src/components/Header/ShoppingCartButton/index.js
@@ -1,9 +1,12 @@
 import { ShoppingCartWrapper, Notifications } from './ShoppingCartButton.styled'
 import CartIcon from 'assets/shopping cart icon.svg'
-import { useSelector, useDispatch } from "react-redux";
-import { addItem, itemCountSelector } from "redux/slices/shoppingCart";
+import { useSelector } from "react-redux";
+import distinctItemsCountSelector from 'redux/slices/shoppingCart';
 
 export default function ShoppingCartButton({ notification, ...props }) {
+  const items = useSelector((state) => state.shoppingCart.items.length);
+
+  console.log(items)
   return (
     <ShoppingCartWrapper tabIndex='0'>
       <img width='28px' height='28px' src={CartIcon} alt="Shopping Cart Icon" />
