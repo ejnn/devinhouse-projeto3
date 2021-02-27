8f39d4e4348ae30bd86bfd7d19f8e2e1d49b9e13
GuiNoronhaS <guinoronha7@gmail.com>
2021-02-27 19:31:51 -0300

    BackButton Wrapper

--- src/pages/ProductDetailsPage/GoBackButton/index.js
+++ src/pages/ProductDetailsPage/GoBackButton/index.js
@@ -1,13 +1,16 @@
 import { useHistory } from 'react-router-dom';
 
 import Button from "components/Button";
+import { GoBackWrapper } from './GoBackButton.styled';
 
 const GoBackButton = () => {
 
   const history = useHistory();
-  
+
   return (
-      <Button onClick={history.goBack}>Voltar</Button>
+      <GoBackWrapper>
+        <Button onClick={history.goBack}>Voltar</Button>
+      </GoBackWrapper>
   );
 }
 export default GoBackButton;
