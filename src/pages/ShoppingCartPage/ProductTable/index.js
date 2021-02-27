a18fb364a3805496b5c8bc51568d5f0d0f5f7707
ejnn <eduardojorgenicolau@gmail.com>
2021-02-27 17:00:25 -0300

    ProductTable behavior encapsulation

--- src/pages/ShoppingCartPage/ProductTable/index.js
+++ src/pages/ShoppingCartPage/ProductTable/index.js
@@ -1,13 +1,30 @@
+import { Fragment } from "react";
+
 import TableHeader from "./TableHeader";
 import TableBody from "./TableBody";
+import ProductRow from "./ProductRow";
+import RowSeparator from "./RowSeparator";
 
-const ProductTable = ({ children }) => {
+const ProductTable = ({ products }) => {
 
     return (
 	<>
 	    <TableHeader/>
 	    <TableBody>
-		{children}
+		{
+		    products.map((productData, index) => {
+			if (index == 0) {
+			    return <ProductRow key={productData.id} productData={productData}/>;
+			} else {
+			    return (
+				<Fragment key={productData.id}>
+				    <RowSeparator/>
+				    <ProductRow key={productData.id} productData={productData}/>
+				</Fragment>
+			    );
+			}
+		    })
+		}
 	    </TableBody>
 	</>
     );
