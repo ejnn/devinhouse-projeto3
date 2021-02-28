72d61a86af5d12722207dbd002c21d2f1e7abfb5
gustavoluchi <gustavo.luchi@gmail.com>
2021-02-28 14:22:40 -0300

    estilizacao do esqueleto

---
+++ src/components/ProductsGridSkeleton/ProductsGridSkeleton.styled.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const PriceWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  > * {
+    margin-bottom: 4px;
+  }
+`;
