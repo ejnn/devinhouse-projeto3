5596d5a35c7215f121544c8b0f3250b1fdbd139f
gustavoluchi <gustavo.luchi@gmail.com>
2021-03-01 14:14:44 -0300

    troquei todos links das imagens pra https

--- src/utils/api.js
+++ src/utils/api.js
@@ -25,8 +25,8 @@ const PRODUCTS_MOCK = [
     description:
       'Compatible with select electronic devices; AAA size; DURALOCK Power Preserve technology; 4-pack',
     url:
-      'http://www.bestbuy.com/site/duracell-aaa-batteries-4-pack/43900.p?id=1051384074145&skuId=43900&cmp=RMXCC',
-    image: 'http://img.bbystatic.com/BestBuy_US/images/products/4390/43900_sa.jpg',
+      'https://www.bestbuy.com/site/duracell-aaa-batteries-4-pack/43900.p?id=1051384074145&skuId=43900&cmp=RMXCC',
+    image: 'https://img.bbystatic.com/BestBuy_US/images/products/4390/43900_sa.jpg',
   },
   {
     id: 48530,
@@ -35,8 +35,8 @@ const PRODUCTS_MOCK = [
     description:
       'Long-lasting energy; DURALOCK Power Preserve technology; for toys, clocks, radios, games, remotes, PDAs and more',
     url:
-      'http://www.bestbuy.com/site/duracell-aa-1-5v-coppertop-batteries-4-pack/48530.p?id=1099385268988&skuId=48530&cmp=RMXCC',
-    image: 'http://img.bbystatic.com/BestBuy_US/images/products/4853/48530_sa.jpg',
+      'https://www.bestbuy.com/site/duracell-aa-1-5v-coppertop-batteries-4-pack/48530.p?id=1099385268988&skuId=48530&cmp=RMXCC',
+    image: 'https://img.bbystatic.com/BestBuy_US/images/products/4853/48530_sa.jpg',
   },
   {
     id: 127687,
@@ -45,8 +45,8 @@ const PRODUCTS_MOCK = [
     description:
       'Compatible with select electronic devices; AA size; DURALOCK Power Preserve technology; 8-pack',
     url:
-      'http://www.bestbuy.com/site/duracell-aa-batteries-8-pack/127687.p?id=1051384045676&skuId=127687&cmp=RMXCC',
-    image: 'http://img.bbystatic.com/BestBuy_US/images/products/1276/127687_sa.jpg',
+      'https://www.bestbuy.com/site/duracell-aa-batteries-8-pack/127687.p?id=1051384045676&skuId=127687&cmp=RMXCC',
+    image: 'https://img.bbystatic.com/BestBuy_US/images/products/1276/127687_sa.jpg',
   },
   {
     id: 150115,
@@ -54,8 +54,8 @@ const PRODUCTS_MOCK = [
     price: 4.99,
     description: '4-pack AA alkaline batteries; battery tester included',
     url:
-      'http://www.bestbuy.com/site/energizer-max-batteries-aa-4-pack/150115.p?id=1051384046217&skuId=150115&cmp=RMXCC',
-    image: 'http://img.bbystatic.com/BestBuy_US/images/products/1501/150115_sa.jpg',
+      'https://www.bestbuy.com/site/energizer-max-batteries-aa-4-pack/150115.p?id=1051384046217&skuId=150115&cmp=RMXCC',
+    image: 'https://img.bbystatic.com/BestBuy_US/images/products/1501/150115_sa.jpg',
   },
   {
     id: 185230,
@@ -64,8 +64,8 @@ const PRODUCTS_MOCK = [
     description:
       'Compatible with select electronic devices; C size; DURALOCK Power Preserve technology; 4-pack',
     url:
-      'http://www.bestbuy.com/site/duracell-c-batteries-4-pack/185230.p?id=1051384046486&skuId=185230&cmp=RMXCC',
-    image: 'http://img.bbystatic.com/BestBuy_US/images/products/1852/185230_sa.jpg',
+      'https://www.bestbuy.com/site/duracell-c-batteries-4-pack/185230.p?id=1051384046486&skuId=185230&cmp=RMXCC',
+    image: 'https://img.bbystatic.com/BestBuy_US/images/products/1852/185230_sa.jpg',
   },
   {
     id: 185267,
@@ -74,8 +74,8 @@ const PRODUCTS_MOCK = [
     description:
       'Compatible with select electronic devices; D size; DURALOCK Power Preserve technology; 4-pack',
     url:
-      'http://www.bestbuy.com/site/duracell-d-batteries-4-pack/185267.p?id=1051384046551&skuId=185267&cmp=RMXCC',
-    image: 'http://img.bbystatic.com/BestBuy_US/images/products/1852/185267_sa.jpg',
+      'https://www.bestbuy.com/site/duracell-d-batteries-4-pack/185267.p?id=1051384046551&skuId=185267&cmp=RMXCC',
+    image: 'https://img.bbystatic.com/BestBuy_US/images/products/1852/185267_sa.jpg',
   },
   {
     id: 312290,
@@ -84,8 +84,8 @@ const PRODUCTS_MOCK = [
     description:
       'Compatible with select electronic devices; alkaline chemistry; 9V size; DURALOCK Power Preserve technology; 2-pack',
     url:
-      'http://www.bestbuy.com/site/duracell-9v-batteries-2-pack/312290.p?id=1051384050321&skuId=312290&cmp=RMXCC',
-    image: 'http://img.bbystatic.com/BestBuy_US/images/products/3122/312290_sa.jpg',
+      'https://www.bestbuy.com/site/duracell-9v-batteries-2-pack/312290.p?id=1051384050321&skuId=312290&cmp=RMXCC',
+    image: 'https://img.bbystatic.com/BestBuy_US/images/products/3122/312290_sa.jpg',
   },
   {
     id: 324884,
@@ -94,8 +94,8 @@ const PRODUCTS_MOCK = [
     description:
       'From our expanded online assortment; compatible with Directed Electronics alarm systems; microphone and microprocessor detect and analyze intrusions; detects quiet glass breaks',
     url:
-      'http://www.bestbuy.com/site/directed-electronics-viper-audio-glass-break-sensor/324884.p?id=1112808077651&skuId=324884&cmp=RMXCC',
-    image: 'http://img.bbystatic.com/BestBuy_US/images/products/3248/324884_rc.jpg',
+      'https://www.bestbuy.com/site/directed-electronics-viper-audio-glass-break-sensor/324884.p?id=1112808077651&skuId=324884&cmp=RMXCC',
+    image: 'https://img.bbystatic.com/BestBuy_US/images/products/3248/324884_rc.jpg',
   },
   {
     id: 333179,
@@ -103,8 +103,8 @@ const PRODUCTS_MOCK = [
     price: 5.99,
     description: 'Alkaline batteries; 1.5V',
     url:
-      'http://www.bestbuy.com/site/energizer-n-cell-e90-batteries-2-pack/333179.p?id=1185268509951&skuId=333179&cmp=RMXCC',
-    image: 'http://img.bbystatic.com/BestBuy_US/images/products/3331/333179_sa.jpg',
+      'https://www.bestbuy.com/site/energizer-n-cell-e90-batteries-2-pack/333179.p?id=1185268509951&skuId=333179&cmp=RMXCC',
+    image: 'https://img.bbystatic.com/BestBuy_US/images/products/3331/333179_sa.jpg',
   },
   {
     id: 346575,
@@ -114,8 +114,8 @@ const PRODUCTS_MOCK = [
     description:
       'From our expanded online assortment; compatible with most 1989-2000 Ford, Lincoln and Mercury vehicles; snap-in TurboKit offers fast installation; spacer/trim ring; rear support bracket',
     url:
-      'http://www.bestbuy.com/site/metra-radio-installation-dash-kit-for-most-1989-2000-ford-lincoln-mercury-vehicles-black/346575.p?id=1218118704590&skuId=346575&cmp=RMXCC',
-    image: 'http://img.bbystatic.com/BestBuy_US/images/products/3465/346575_rc.jpg',
+      'https://www.bestbuy.com/site/metra-radio-installation-dash-kit-for-most-1989-2000-ford-lincoln-mercury-vehicles-black/346575.p?id=1218118704590&skuId=346575&cmp=RMXCC',
+    image: 'https://img.bbystatic.com/BestBuy_US/images/products/3465/346575_rc.jpg',
   },
   {
     id: 346646,
@@ -124,8 +124,8 @@ const PRODUCTS_MOCK = [
     description:
       'From our expanded online assortment; compatible with select GM vehicles; plastic material',
     url:
-      'http://www.bestbuy.com/site/metra-radio-dash-multikit-for-select-gm-vehicles-black/346646.p?id=1210376657731&skuId=346646CC',
-    image: 'http://img.bbystatic.com/BestBuy_US/images/products/3466/346646_rc.jpg',
+      'https://www.bestbuy.com/site/metra-radio-dash-multikit-for-select-gm-vehicles-black/346646.p?id=1210376657731&skuId=346646CC',
+    image: 'https://img.bbystatic.com/BestBuy_US/images/products/3466/346646_rc.jpg',
   },
   {
     id: 347137,
@@ -134,8 +134,8 @@ const PRODUCTS_MOCK = [
     description:
       "Compatible with select 1998-2008 Ford vehicles; connects an aftermarket radio to a vehicle's harness",
     url:
-      'http://www.bestbuy.com/site/metra-wiring-harness-for-select-1998-2008-ford-vehicles-multicolored/347137.p?id=1142290459780&skuId=347137&cmp=RMXCC',
-    image: 'http://img.bbystatic.com/BestBuy_US/images/products/9852/9852688_sa.jpg',
+      'https://www.bestbuy.com/site/metra-wiring-harness-for-select-1998-2008-ford-vehicles-multicolored/347137.p?id=1142290459780&skuId=347137&cmp=RMXCC',
+    image: 'https://img.bbystatic.com/BestBuy_US/images/products/9852/9852688_sa.jpg',
   },
   {
     id: 347146,
@@ -144,8 +144,8 @@ const PRODUCTS_MOCK = [
     description:
       "Compatible with Honda and Acura vehicles; connects an aftermarket radio to your car's harness",
     url:
-      'http://www.bestbuy.com/site/metra-turbo-wire-aftermarket-radio-wire-harness-adapter-for-select-vehicles/347146.p?id=1183160746244&skuId=347146&cmp=RMXCC',
-    image: 'http://img.bbystatic.com/BestBuy_US/images/products/3471/347146_rc.jpg',
+      'https://www.bestbuy.com/site/metra-turbo-wire-aftermarket-radio-wire-harness-adapter-for-select-vehicles/347146.p?id=1183160746244&skuId=347146&cmp=RMXCC',
+    image: 'https://img.bbystatic.com/BestBuy_US/images/products/3471/347146_rc.jpg',
   },
   {
     id: 347155,
@@ -154,8 +154,8 @@ const PRODUCTS_MOCK = [
     description:
       "Compatible with most 1986-1998 Honda Acura vehicles; connects an aftermarket radio to a vehicle's harness",
     url:
-      'http://www.bestbuy.com/site/metra-wiring-harness-for-most-1986-1998-honda-acura-vehicles-multicolored/347155.p?id=1142292396747&skuId=347155&cmp=RMXCC',
-    image: 'http://img.bbystatic.com/BestBuy_US/images/products/3471/347155_rc.jpg',
+      'https://www.bestbuy.com/site/metra-wiring-harness-for-most-1986-1998-honda-acura-vehicles-multicolored/347155.p?id=1142292396747&skuId=347155&cmp=RMXCC',
+    image: 'https://img.bbystatic.com/BestBuy_US/images/products/3471/347155_rc.jpg',
   },
   {
     id: 347333,
@@ -164,8 +164,8 @@ const PRODUCTS_MOCK = [
     description:
       'Compatible with select 1988-2005 vehicles; adapts an aftermarket antenna to OEM radios; flat plug',
     url:
-      'http://www.bestbuy.com/site/metra-antenna-cable-adapter-black/347333.p?id=1169512349334&skuId=347333&cmp=RMXCC',
-    image: 'http://img.bbystatic.com/BestBuy_US/images/products/3473/347333_sa.jpg',
+      'https://www.bestbuy.com/site/metra-antenna-cable-adapter-black/347333.p?id=1169512349334&skuId=347333&cmp=RMXCC',
+    image: 'https://img.bbystatic.com/BestBuy_US/images/products/3473/347333_sa.jpg',
   },
   {
     id: 349572,
@@ -173,9 +173,9 @@ const PRODUCTS_MOCK = [
     price: 29.99,
     description: 'PORTABLE RADAR DETECTOR INST',
     url:
-      'http://www.bestbuy.com/site/install-portable-radar-detector-inst/349572.p?id=1218019625086&skuId=349572&cmp=RMXCC',
+      'https://www.bestbuy.com/site/install-portable-radar-detector-inst/349572.p?id=1218019625086&skuId=349572&cmp=RMXCC',
     image:
-      'http://img.bbystatic.com/BestBuy_US/images/products/nonsku/default_hardlines_m.gif',
+      'https://img.bbystatic.com/BestBuy_US/images/products/nonsku/default_hardlines_m.gif',
   },
   {
     id: 373642,
@@ -184,8 +184,8 @@ const PRODUCTS_MOCK = [
     description:
       'Rechargeable 3.6V 300 mAh NiCad battery for GE 2-9614 model cordless phones',
     url:
-      'http://www.bestbuy.com/site/jensen-3-6v-nicad-battery-for-900mhz-phones/373642.p?id=1051384793767&skuId=373642&cmp=RMXCC',
-    image: 'http://img.bbystatic.com/BestBuy_US/images/products/3736/373642_rc.jpg',
+      'https://www.bestbuy.com/site/jensen-3-6v-nicad-battery-for-900mhz-phones/373642.p?id=1051384793767&skuId=373642&cmp=RMXCC',
+    image: 'https://img.bbystatic.com/BestBuy_US/images/products/3736/373642_rc.jpg',
   },
   {
     id: 478398,
@@ -195,8 +195,8 @@ const PRODUCTS_MOCK = [
     description:
       'From our expanded online assortment; compatible with select Ford, Mazda and Mercury vehicles; allows the installation of an aftermarket radio into the factory dash location; high-grade ABS plastic material',
     url:
-      'http://www.bestbuy.com/site/metra-radio-installation-dash-kit-for-select-ford-mazda-and-mercury-vehicles-pair-black/478398.p?id=1185268614282&skuId=478398&cmp=RMXCC',
-    image: 'http://img.bbystatic.com/BestBuy_US/images/products/4783/478398_rc.jpg',
+      'https://www.bestbuy.com/site/metra-radio-installation-dash-kit-for-select-ford-mazda-and-mercury-vehicles-pair-black/478398.p?id=1185268614282&skuId=478398&cmp=RMXCC',
+    image: 'https://img.bbystatic.com/BestBuy_US/images/products/4783/478398_rc.jpg',
   },
   {
     id: 612732,
@@ -205,8 +205,8 @@ const PRODUCTS_MOCK = [
     description:
       'Compatible with most vehicles; designed for the installation of an aftermarket radio',
     url:
-      'http://www.bestbuy.com/site/metra-1-4-din-trim-ring-for-most-vehicles/612732.p?id=1179531744934&skuId=612732&cmp=RMXCC',
-    image: 'http://img.bbystatic.com/BestBuy_US/images/products/6127/612732_sa.jpg',
+      'https://www.bestbuy.com/site/metra-1-4-din-trim-ring-for-most-vehicles/612732.p?id=1179531744934&skuId=612732&cmp=RMXCC',
+    image: 'https://img.bbystatic.com/BestBuy_US/images/products/6127/612732_sa.jpg',
   },
   {
     id: 643600,
@@ -215,8 +215,8 @@ const PRODUCTS_MOCK = [
     description:
       'Compatible with select Jeep vehicles; ABS and vinyl construction; colored-coded wires',
     url:
-      'http://www.bestbuy.com/site/metra-turbowire-radio-harness-adapter-for-select-jeep-vehicles/643600.p?id=1183160746436&skuId=643600&cmp=RMXCC',
-    image: 'http://img.bbystatic.com/BestBuy_US/images/products/6436/643600_rc.jpg',
+      'https://www.bestbuy.com/site/metra-turbowire-radio-harness-adapter-for-select-jeep-vehicles/643600.p?id=1183160746436&skuId=643600&cmp=RMXCC',
+    image: 'https://img.bbystatic.com/BestBuy_US/images/products/6436/643600_rc.jpg',
   },
   {
     id: 643628,
@@ -225,8 +225,8 @@ const PRODUCTS_MOCK = [
     description:
       "Compatible with select Volkswagen vehicles; connects a speaker to the vehicle's stereo; easy installation",
     url:
-      'http://www.bestbuy.com/site/metra-speaker-connector-for-select-volkswagen-vehicles/643628.p?id=1183160746500&skuId=643628&cmp=RMXCC',
-    image: 'http://img.bbystatic.com/BestBuy_US/images/products/6436/643628_rc.jpg',
+      'https://www.bestbuy.com/site/metra-speaker-connector-for-select-volkswagen-vehicles/643628.p?id=1183160746500&skuId=643628&cmp=RMXCC',
+    image: 'https://img.bbystatic.com/BestBuy_US/images/products/6436/643628_rc.jpg',
   },
   {
     id: 643691,
@@ -235,8 +235,8 @@ const PRODUCTS_MOCK = [
     description:
       'From our expanded online assortment; compatible with 1987 - 1993 Mitsubishi and Chrysler vehicles; color-coded wires',
     url:
-      'http://www.bestbuy.com/site/metra-speaker-connector-for-select-mitsubishi-and-chrysler-vehicles/643691.p?id=1218019642861&skuId=643691&cmp=RMXCC',
-    image: 'http://img.bbystatic.com/BestBuy_US/images/products/6436/643691_rc.jpg',
+      'https://www.bestbuy.com/site/metra-speaker-connector-for-select-mitsubishi-and-chrysler-vehicles/643691.p?id=1218019642861&skuId=643691&cmp=RMXCC',
+    image: 'https://img.bbystatic.com/BestBuy_US/images/products/6436/643691_rc.jpg',
   },
   {
     id: 643717,
@@ -245,8 +245,8 @@ const PRODUCTS_MOCK = [
     description:
       "Compatible with most 1990-2001 Mazda vehicles; connects an aftermarket radio to a vehicle's harness",
     url:
-      'http://www.bestbuy.com/site/metra-wiring-harness-for-most-1990-2001-mazda-vehicles-multicolored/643717.p?id=1142291937563&skuId=643717&cmp=RMXCC',
-    image: 'http://img.bbystatic.com/BestBuy_US/images/products/6437/643717_rc.jpg',
+      'https://www.bestbuy.com/site/metra-wiring-harness-for-most-1990-2001-mazda-vehicles-multicolored/643717.p?id=1142291937563&skuId=643717&cmp=RMXCC',
+    image: 'https://img.bbystatic.com/BestBuy_US/images/products/6437/643717_rc.jpg',
   },
   {
     id: 673890,
@@ -255,7 +255,7 @@ const PRODUCTS_MOCK = [
     description:
       'From our expanded online assortment; compatible with select Chevrolet, Geo and Suzuki vehicles; allows installation of an aftermarket radio; provision for a 1/4" or 1/2" DIN equalizer; mounts precisely to OEM radio mounting positions',
     url:
-      'http://www.bestbuy.com/site/metra-professional-installer-series-turbokit/673890.p?id=1118842643714&skuId=673890&cmp=RMXCC',
-    image: 'http://img.bbystatic.com/BestBuy_US/images/products/9852/9852688_sa.jpg',
+      'https://www.bestbuy.com/site/metra-professional-installer-series-turbokit/673890.p?id=1118842643714&skuId=673890&cmp=RMXCC',
+    image: 'https://img.bbystatic.com/BestBuy_US/images/products/9852/9852688_sa.jpg',
   },
 ];
